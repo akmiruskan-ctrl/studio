92665c23146ee0fe1a56f10f2a874ae29789f917
akmi ruskan <akmiruskan@gmail.com>
2025-12-02 03:35:09 +0000

    untuk poto berita dinkes oku tampilkan ini: https://img.antaranews.com/c

--- next.config.ts
+++ next.config.ts
@@ -28,8 +28,16 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.antaranews.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
 
 export default nextConfig;
+
+    
